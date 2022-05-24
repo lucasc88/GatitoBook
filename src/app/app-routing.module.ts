dc328c84e0efea2ef2966a6e4950bcc93c37f7a2
Lucas <lucasdec88@gmail.com>
2022-05-24 15:57:24 +0100

    Login Component has been created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+//The first Routes position is to define the home page(without any resource)
+//for exemple: empty path or blank space in the path will be redirected to home page
+//loadChildren: () has LazyLoad behaviour
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home'
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
