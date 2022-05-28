69bce7cefd5ee0c3725a42c415a70b1c73224aec
Lucas <lucasdec88@gmail.com>
2022-05-28 15:21:34 +0100

    Header Component has been implemented

---
+++ src/app/component/header/header.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header.component';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  declarations: [HeaderComponent],
+  imports: [CommonModule, RouterModule],
+  exports: [HeaderComponent] //Exports means who import HeaderModule, can use the HeaderComponent
+})
+export class HeaderModule { }
