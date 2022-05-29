495d2b9be74ca6856b7c270fc022442a275382ee
Lucas <lucasdec88@gmail.com>
2022-05-29 12:27:31 +0100

    Menu Component added to Header Component

--- src/app/component/header/header.module.ts
+++ src/app/component/header/header.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header.component';
 import { RouterModule } from '@angular/router';
+import { MenuModule } from '../menu/menu.module';
 
 
 //Exports means who import HeaderModule, can use the HeaderComponent
 @NgModule({
   declarations: [HeaderComponent],
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, MenuModule],
   exports: [HeaderComponent] 
 })
 export class HeaderModule { }
