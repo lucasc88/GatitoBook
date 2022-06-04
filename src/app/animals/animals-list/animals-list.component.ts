0b45264ef71e5ac55ce7ef01f58c0784a3ca8743
Lucas <lucasdec88@gmail.com>
2022-06-04 12:25:07 +0100

    Pipe async used instead subscribe inside itself

--- src/app/animals/animals-list/animals-list.component.ts
+++ src/app/animals/animals-list/animals-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, switchMap } from 'rxjs';
 import { UserService } from 'src/app/autentication/user/user.service';
 import { Animals } from '../animals';
 import { AnimalsService } from '../animals.service';
@@ -10,18 +11,31 @@ import { AnimalsService } from '../animals.service';
 })
 export class AnimalsListComponent implements OnInit {
 
-  animals!: Animals;
+  //$ at the end of variable is just to inditify that is an Observable
+  animals$!: Observable<Animals>;
 
   constructor(private userService: UserService, private animalsService: AnimalsService) { }
 
+  //it's not a good pratice to do a subscribe inside other subscribe
+  //RXJS is the best way. The data flow is a kind of pipe and we can use RXJS to simplify.
   ngOnInit(): void {
-    this.userService.returnUser().subscribe((user) => {
-      //in case user.name is undefined or null, ?? will put an empty string
-      const userName = user.name ?? '';
-      this.animalsService.userList(userName).subscribe((animals) => {
-        this.animals = animals;
+    // this.userService.returnUser().subscribe((user) => {
+    //   //in case user.name is undefined or null, ?? will put an empty string
+    //   const userName = user.name ?? '';
+    //   this.animalsService.userList(userName).subscribe((animals) => {
+    //     this.animals = animals;
+    //   })
+    // });
+
+    //we can use operators inside the pipe
+    //switchMap will change the flow from User to Animals 
+    this.animals$ = this.userService.returnUser().pipe(
+      switchMap((user) => {
+        const userName = user.name ?? '';
+        return this.animalsService.userList(userName);//switchMap requires return another Observable
+        //using Observable fully, we can use pipe async. In this way, Angular will do Subscribe and Unsubscribe automatically
       })
-    });
+    );
   }
 
 }
